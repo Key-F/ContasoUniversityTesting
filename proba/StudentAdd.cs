828039b51c9aba81a144d19a7d725a50712e30cb
Key-F <kirill2010.97@mail.ru>
2018-06-23 20:14:16 +0300

    +StudentSearch Test
    Тест на добавление ученика теперь обходится без поиска, тк тестовые значения выводятся первыми.

--- proba/StudentAdd.cs
+++ proba/StudentAdd.cs
@@ -9,25 +9,25 @@ namespace proba
     [TestClass]
     public class StudentAdd
     {
-        string testFirstName = "TestFirst";
-        string testLastName = "TestLast";
-        string testDate = "15-11-2018"; //
+        string testFirstName = "1TestFirst"; // Значения, чтобы тестовый ученик оказался наверу списка
+        string testLastName = "1TestLast";
+        string testDate = "1-11-2018"; // M-d-yyyy
 
         [TestMethod]
-        public void LogInBanner() // На странице Log in присутствует баннер "Log In"
+        public void StudentAddTest() // На странице Log in присутствует баннер "Log In"
         {
             IWebDriver Dr;
             Dr = new InternetExplorerDriver();
             Dr.Navigate().GoToUrl("https://contoso-university-demo.azurewebsites.net/Students");
-            Dr.FindElement(By.LinkText("Create New")).Click();
+            Dr.FindElement(By.LinkText("Create New")).Click(); // Нажимаем создать нового ученика
             Dr.FindElement(By.CssSelector(".form-horizontal div:nth-child(2) input")).SendKeys(testLastName); // Заполняем поле Last Name
             Dr.FindElement(By.CssSelector(".form-horizontal div:nth-child(4) input")).SendKeys(testFirstName); // Заполняем поле First Name           
             Dr.FindElement(By.CssSelector("input#EnrollmentDate.form-control")).SendKeys(testDate + Keys.Enter); // Заполняем поле Enrollment Date           
             Thread.Sleep(1000);
             //Dr.FindElement(By.CssSelector(".col-md-offset-2.col-md-10 input")).Click(); // Нажимаем "Create"
-            Dr.FindElement(By.Name("SearchString")).SendKeys(testFirstName + Keys.Enter); // Поиск по имени
-            Thread.Sleep(1000);         
-            Assert.IsTrue(Dr.FindElement(By.CssSelector("tbody tr td:nth-child(2)")).Text == testFirstName); // Ищем созданного по имени
+            //Dr.FindElement(By.Name("SearchString")).SendKeys(testFirstName + Keys.Enter); // Поиск по имени
+            // Thread.Sleep(1000);         
+            Assert.IsTrue(Dr.FindElement(By.CssSelector("tbody tr td:nth-child(2)")).Text == testFirstName); // Ищем созданного по имени на странице
             Dr.FindElement(By.CssSelector(".table a:nth-child(3)")).Click(); // Чистим за собой
             Dr.FindElement(By.CssSelector("input.btn.btn-default")).Click(); // Подтверждение удаления            
             Dr.Quit();
---
+++ proba/StudentSearch.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using System.Threading;
+
+namespace proba
+{
+    [TestClass]
+    public class StudentSearch
+    {
+        string testFirstName = "TestFirst";
+        string testLastName = "TestLast";
+        string testDate = "1-11-2018"; // M-d-yyyy
+
+        [TestMethod]
+        public void SearchFirstNameTest() // В поле поиска вводим имя ученика
+        {
+            IWebDriver Dr;
+            Dr = new InternetExplorerDriver();
+            Dr.Navigate().GoToUrl("https://contoso-university-demo.azurewebsites.net/Students");
+           // string testName = Dr.FindElement(By.CssSelector("tbody tr td:nth-child(2)")).Text;
+            Dr.FindElement(By.LinkText("Create New")).Click(); // Нажимаем создать нового ученика
+            Dr.FindElement(By.CssSelector(".form-horizontal div:nth-child(2) input")).SendKeys(testLastName); // Заполняем поле Last Name
+            Dr.FindElement(By.CssSelector(".form-horizontal div:nth-child(4) input")).SendKeys(testFirstName); // Заполняем поле First Name           
+            Dr.FindElement(By.CssSelector("input#EnrollmentDate.form-control")).SendKeys(testDate + Keys.Enter); // Заполняем поле Enrollment Date           
+            Thread.Sleep(1000);
+            //Dr.FindElement(By.CssSelector(".col-md-offset-2.col-md-10 input")).Click(); // Нажимаем "Create"
+            Dr.FindElement(By.Name("SearchString")).SendKeys(testFirstName + Keys.Enter); // Поиск по имени
+            Thread.Sleep(1000);
+            Assert.IsTrue(Dr.FindElement(By.CssSelector("tbody tr td:nth-child(1)")).Text == testLastName); // Ищем созданную фамилию на странице
+            Dr.FindElement(By.CssSelector(".table a:nth-child(3)")).Click(); // Чистим за собой
+            Dr.FindElement(By.CssSelector("input.btn.btn-default")).Click(); // Подтверждение удаления            
+            Dr.Quit();
+        }
+
+        [TestMethod]
+        public void SearchLastNameTest() // В поле поиска вводим фамилию ученика
+        {
+            IWebDriver Dr;
+            Dr = new InternetExplorerDriver();
+            Dr.Navigate().GoToUrl("https://contoso-university-demo.azurewebsites.net/Students");
+            // string testName = Dr.FindElement(By.CssSelector("tbody tr td:nth-child(2)")).Text;
+            Dr.FindElement(By.LinkText("Create New")).Click(); // Нажимаем создать нового ученика
+            Dr.FindElement(By.CssSelector(".form-horizontal div:nth-child(2) input")).SendKeys(testLastName); // Заполняем поле Last Name
+            Dr.FindElement(By.CssSelector(".form-horizontal div:nth-child(4) input")).SendKeys(testFirstName); // Заполняем поле First Name           
+            Dr.FindElement(By.CssSelector("input#EnrollmentDate.form-control")).SendKeys(testDate + Keys.Enter); // Заполняем поле Enrollment Date           
+            Thread.Sleep(1000);
+            //Dr.FindElement(By.CssSelector(".col-md-offset-2.col-md-10 input")).Click(); // Нажимаем "Create"
+            Dr.FindElement(By.Name("SearchString")).SendKeys(testFirstName + Keys.Enter); // Поиск по фамилии
+            Thread.Sleep(1000);
+            Assert.IsTrue(Dr.FindElement(By.CssSelector("tbody tr td:nth-child(2)")).Text == testFirstName); // Ищем созданное имя на странице
+            Dr.FindElement(By.CssSelector(".table a:nth-child(3)")).Click(); // Чистим за собой
+            Dr.FindElement(By.CssSelector("input.btn.btn-default")).Click(); // Подтверждение удаления            
+            Dr.Quit();
+        }
+    }
+}
